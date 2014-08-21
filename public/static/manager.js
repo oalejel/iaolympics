9999ba146afcc4b52fe97613bfacfa74c3942220
Amit Mizrahi <amitmizrahi10@gmail.com>
2014-08-21 09:37:37 -0400

    make progress on script

--- public/static/manager.js
+++ public/static/manager.js
@@ -1,3 +1,36 @@
 $(function() {
-	
+	var highestFormNumber = 1;
+
+	$("#addform").click(function() {
+
+		highestFormNumber++;
+
+		var newForm = document.createElement("form");
+		newForm.setAttribute("action", "");
+		newForm.setAttribute("method", "post");
+		newForm.setAttribute("id", "form"+highestFormNumber);
+		newForm.setAttribute("style", "display:none;");
+
+		document.getElementById("forms").appendChild(newForm);
+
+		// add input fields
+
+		var yearInput = document.createElement("input");
+		yearInput.setAttribute("type", "text");
+		yearInput.setAttribute("name", "year");
+		yearInput.setAttribute("placeholder", "Class year");
+
+		var nameInput = document.createElement("input");
+		nameInput.setAttribute("type", "text");
+		nameInput.setAttribute("name", "name");
+		nameInput.setAttribute("placeholder", "Class name (e.g. seniors, juniors)");
+
+		var colorInput = document.createElement("input");
+		
+
+		newForm.appendChild(yearInput);
+		newForm.appendChild(nameInput);
+
+		$("#form"+highestFormNumber).fadeIn();
+	});
 });
