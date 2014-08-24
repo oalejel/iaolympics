119d3a6bbb56112235382fc235aa531ecae8128f
Amit Mizrahi <amitmizrahi10@gmail.com>
2014-08-24 00:02:22 -0400

    implement scoreboard/sliding bar

--- public/static/scoreboard.js
+++ public/static/scoreboard.js
@@ -1,3 +1,25 @@
 $(function() {
 	// ...
+
+	var Scoreboard = function(scores) {
+		this.scores = scores;
+		this.highestScore = this.max(scores);
+	}
+
+	Scoreboard.prototype.max = function(arr) {
+		var highest = -Infinity;
+		for(var i = 0; i < arr.length; i++) {
+			if(arr[i] > highest) {
+				highest = arr[i];
+			}
+		}
+		return highest;
+	}
+
+	Scoreboard.prototype.createSlidebar = function(domElement, score) {
+		// takes a div and stretches it (with animation) to the appropriate score width
+		var relativeWidth = (score*1.0)/this.highestScore;
+		var percentageString = (relativeWidth*100).toString() + "%";
+		$(domElement).animate({'width': percentageString}, 1000);
+	}
 });
