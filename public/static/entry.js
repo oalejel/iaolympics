5a56c7d4592e2f080282051cd0acd4d48f62549b
Amit Mizrahi <amitmizrahi10@gmail.com>
2014-08-25 07:26:30 -0400

    basic score entry form + fading in/out

--- public/static/entry.js
+++ public/static/entry.js
@@ -1,3 +1,15 @@
 $(function() {
 
+	$("#add-score-form").hide();
+	$("#deduct-form").hide();
+	$("#rescore-form").hide();
+
+	$(".action").click(function() {
+		$(this).parent().parent().parent().fadeOut(500);
+		$(".centered").fadeOut(500);
+	});
+
+	$("#add-scores").click(function() {
+		$("#add-score-form").fadeIn();
+	});
 });
